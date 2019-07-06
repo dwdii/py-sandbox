8aa075478285cf8406fd7c1ebf9f4194b675d745
dwdii <daniel@dittenhafer.net>
2019-07-05 22:38:08 -0400

    array instead of heap, much better load perf

--- src/shortest-paths/traveling-salesman-nn.py
+++ src/shortest-paths/traveling-salesman-nn.py
@@ -55,25 +55,17 @@ class traveling_salesman_nn:
             nd = self.infinity
             ni = 0
             #start = timer()
-            #for p in remaining:
-            #    if visited.has_key(p):
-            #        continue
-            #    else:
-            #        cd = D[ci][p]
-            #        if cd < nd:
-            #            ni = p
-            #            nd = cd
-            found = False
-            while not found:
-                cd, p = D[ci].pop()
+            for p in remaining:
+                ip = p - 1
                 if visited.has_key(p):
                     continue
                 else:
-                    ni = p
-                    nd = math.sqrt(cd)
-                    found = True
+                    cd = D[ci][ip]
+                    if cd < nd:
+                        ni = p
+                        nd = cd
 
-            total += nd
+            total += math.sqrt(nd)
             visited[ni] = 1
             remaining.remove(ni)
             ci = ni
@@ -84,8 +76,8 @@ class traveling_salesman_nn:
             #print "Visited: {0} in {1} secs".format(len(visited), end - start)
 
         # Go back to source
-        #cd = D[ci][s]
-        cd = D[ci].remove(s)
+        cd = D[ci][s - 1]
+        #cd = D[ci].remove(s)
         total += math.sqrt(cd)
 
         return int(round(total-0.5))
@@ -110,22 +102,14 @@ class traveling_salesman_nn:
 
 
         for p in P:
-            #D[p] = {}
-            D[p] = src.utillib.myheap.heapqplus()
+            D[p] = []
+            #D[p] = src.utillib.myheap.heapqplus()
             p1 = P[p]
             #start = timer()
-            for d in P:
-                ed = None
-                if D.has_key(d):
-                    ed = D[d].get(p)
-
-                if ed is None:
-                    p2 = P[d]
-                    #ed = math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
-                    ed = math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2)
-
-                D[p].add(d, ed)
-                #D[p][d] =
+            for d in xrange(1, len(P) + 1):
+                p2 = P[d]
+                ed = math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2)
+                D[p].append(ed)
 
             #end = timer()
             #print "Distances for {0} computed in {1} secs".format(p, end - start)
